0145283a8ed9fea8c72723e6d5f26d235053cc57
Michal Szczepanski <michal@vane.pl>
2023-01-19 16:09:45 +0100

    fix: prettier config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const Config = {
-  isProduction: process.env.IS_PRODUCTION === "true",
-  foo: process.env.FOO,
+  isProduction: process.env.IS_PRODUCTION === 'true',
+  foo: process.env.FOO
 };
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import { Config } from "./config";
-const h1 = document.createElement("h1");
+import { Config } from './config';
+const h1 = document.createElement('h1');
 document.body.appendChild(h1);
-h1.innerText = `is production ${Config.isProduction.toString()}, foo value from .env: ${
-  Config.foo || ""
-}`;
+h1.innerText = `is production ${Config.isProduction.toString()}, foo value from .env: ${Config.foo || ''}`;
 
 // Hot reloading
 if (!Config.isProduction) {
-  const ws = new WebSocket("ws://localhost:1234");
+  const ws = new WebSocket('ws://localhost:1234');
   ws.onmessage = () => {
     window.location.reload();
   };
